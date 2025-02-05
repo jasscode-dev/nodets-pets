f9a0b7b37b49b3ec97adba5991775a5853d1b972
Jass <jaquelinesantos.dev@gmail.com>
2025-02-05 20:31:38 -0300

    final project 1.0

--- src/server.ts
+++ src/server.ts
@@ -11,14 +11,14 @@ dotenv.config();
 
 const server = express();
 
-server.set('view engine','mustache');
 server.set('views',path.join(__dirname,'views'));
-server.set('mustache',mustache());
+server.set('view engine','mustache');
+server.engine('mustache',mustache());
 
 server.use(express.static(path.join(__dirname,'../public')))
 server.use(mainRoutes)
 server.use((req,res)=>{
-    res.send('Página não encontrada')
+    res.render('pages/404')
 })
 
 
